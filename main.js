24aae3555221d1a5aed62b7e76dbddd8d423773e
Mary Remo <maryjewel99@gmail.com>
2018-11-26 10:18:49 -0600

    first commit on branch with updated main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+Create an object that represents your pet.
+
+Name property with a string value.
+Species property with a string value.
+Nicknames property with an array value. Array contains strings.
+Age property with an integer value
+
+const pet = {
+    name: "Daisy",
+    species: "dog",
+    nicknames: ['daisy-doo','crazy-daisy'],
+    age: 1
+}
